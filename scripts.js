bafa0009703aab95207662e8401631d1db0e7b53
xccyxx <100075841+xccyxx@users.noreply.github.com>
2024-08-16 12:29:51 -0400

    Add round logic and game-ending condition to disable further gameplay

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 let humanScore = 0;
 let computerScore = 0;
 let round = 1;
+let gameEnded = false;
 
 const getcomputerSelection = () => {
     let randomSelection = Math.floor(Math.random() * 3);
@@ -13,90 +14,69 @@ const getcomputerSelection = () => {
     }
 }
 
-const computerSelection = getcomputerSelection();
+const playRound = (humanSelection, computerSelection) => {
+    let messageContainer = document.querySelector(".message-container");
+    messageContainer.innerHTML = "";
 
+    let roundMsg = document.createElement("p");
+    roundMsg.innerText = `Round ${round}`;
+    roundMsg.style.fontWeight = "bold";
+    messageContainer.appendChild(roundMsg);
 
+    let humanSelectionMsg = document.createElement("p");
+    humanSelectionMsg.innerText = `Your choice: ${humanSelection}`
+    messageContainer.appendChild(humanSelectionMsg);
 
-// const playGame = () => {
+    let computerSelectionMsg = document.createElement("p");
+    computerSelectionMsg.innerText = `Computer's choice: ${computerSelection}`
+    messageContainer.appendChild(computerSelectionMsg);
 
-    let playRound = (humanSelection, computerSelection) => {
-        let messageContainer = document.querySelector(".message-container");
-        messageContainer.innerHTML = "";
+    let roundResult = document.createElement("p");
 
-        let roundMsg = document.createElement("p");
-        roundMsg.innerText = `Round ${round}`;
-        roundMsg.style.fontWeight = "bold";
-        messageContainer.appendChild(roundMsg);
-
-        let humanSelectionMsg = document.createElement("p");
-        humanSelectionMsg.innerText = `Your choice: ${humanSelection}`
-        messageContainer.appendChild(humanSelectionMsg);
-
-        let computerSelectionMsg = document.createElement("p");
-        computerSelectionMsg.innerText = `Computer's choice: ${computerSelection}`
-        messageContainer.appendChild(computerSelectionMsg);
-
-        let roundResult = document.createElement("p");
-
-        if (humanSelection === computerSelection) {
-            roundResult.innerText = "Tie";
-            messageContainer.appendChild(roundResult);
-        }
-        if (
-            (humanSelection === "rock" && computerSelection === "scissors") ||
-            (humanSelection === "paper" && computerSelection === "rock") ||
-            (humanSelection === "scissors" && computerSelection === "paper")
-        ) {
-            roundResult.innerText = `You win! ${humanSelection} beats ${computerSelection}.`;
-            messageContainer.appendChild(roundResult);
-            humanScore++;
-        }
-
-        if (
-            (humanSelection === "rock" && computerSelection === "paper") ||
-            (humanSelection === "paper" && computerSelection === "scissors") ||
-            (humanSelection === "scissors" && computerSelection === "rock")
-        ) {
-            roundResult.innerText = `You lose! ${computerSelection} beats ${humanSelection}.`;
-            messageContainer.appendChild(roundResult);
-            computerScore++;
-        }
-
-            
-        let humanScoreMsg = document.createElement("p");
-        humanScoreMsg.innerText = `Your score: ${humanScore}`
-        messageContainer.appendChild(humanScoreMsg);
-
-        let computerScoreMsg = document.createElement("p");
-        computerScoreMsg.innerText = `Computer's score: ${computerScore}`
-        messageContainer.appendChild(computerScoreMsg);
-
-        round++;
+    if (humanSelection === computerSelection) {
+        roundResult.innerText = "Tie";
+        messageContainer.appendChild(roundResult);
+    } else if (
+        (humanSelection === "rock" && computerSelection === "scissors") ||
+        (humanSelection === "paper" && computerSelection === "rock") ||
+        (humanSelection === "scissors" && computerSelection === "paper")
+    ) {
+        roundResult.innerText = `You win! ${humanSelection} beats ${computerSelection}.`;
+        messageContainer.appendChild(roundResult);
+        humanScore++;
+    } else {
+        roundResult.innerText = `You lose! ${computerSelection} beats ${humanSelection}.`;
+        messageContainer.appendChild(roundResult);
+        computerScore++;
     }
 
-    // for (let i = 0; i < 5; i++) {
-    //     console.log(`\n Round${i + 1}`)
-    
-        // playRound(humanSelection, computerSelection);
+    let humanScoreMsg = document.createElement("p");
+    humanScoreMsg.innerText = `Your score: ${humanScore}`
+    messageContainer.appendChild(humanScoreMsg);
 
-    //     if (i === 4) {
-    //         if (humanScore > computerScore) {
-    //             console.log("The game has ended...\nYou are the winner!")
-    //         } else if (humanScore < computerScore) {
-    //             console.log("The game has ended...\nComputer is the winner.")
-    //         } else {
-    //             console.log("The game has ended...\nDraw.")
-    //         }
-    //     }
-    // }
-// }
-// playGame();
+    let computerScoreMsg = document.createElement("p");
+    computerScoreMsg.innerText = `Computer's score: ${computerScore}`
+    messageContainer.appendChild(computerScoreMsg);
+
+    let finalResult = document.createElement("p");
+    if (humanScore >= 5) {
+        finalResult.innerText = "The game has ended...\nYou are the winner!";
+        gameEnded = true;
+    }
+    if (computerScore >= 5) {
+        finalResult.innerText = "The game has ended...\nComputer is the winner.";
+        gameEnded = true;
+    }
+    messageContainer.appendChild(finalResult);
+    round++;
+}
 
 document.addEventListener("DOMContentLoaded", function() {
-    let body = document.querySelector("body");
-    body.addEventListener("click", (e) => {
-        if (e.target.className === "choice") {
+    let container = document.querySelector(".container");
+    container.addEventListener("click", (e) => {
+        if (e.target.className === "choice" && !gameEnded) {
             const humanSelection = e.target.textContent.toLowerCase();
+            const computerSelection = getcomputerSelection();
             playRound(humanSelection, computerSelection);
         }
     });
