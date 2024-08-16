6fe8f3e8c4671e05ca794df84d7667ad08707f67
xccyxx <100075841+xccyxx@users.noreply.github.com>
2024-08-16 00:50:00 -0400

    Refactor playRound function and update UI interaction
    - Replace console.log calls with DOM manipulation to append messages as HTML elements
    - Update playRound function to display round, game results and scores in the UI
    - Adjust variable scopes in scripts.js

--- scripts.js
+++ scripts.js
@@ -1,70 +1,81 @@
-const getComputerChoice = () => {
-    let computerChoice = Math.floor(Math.random() * 3);
-    if (computerChoice === 0) {
+let humanScore = 0;
+let computerScore = 0;
+let round = 1;
+
+const getcomputerSelection = () => {
+    let randomSelection = Math.floor(Math.random() * 3);
+    if (randomSelection === 0) {
         return "rock";
-    } else if (computerChoice === 1) {
+    } else if (randomSelection === 1) {
         return "paper";
     } else {
         return "scissors";
     }
 }
 
-const getHumanChoice = () => {
-    let humanChoice = prompt("Rock, Paper or Scissors?").toLowerCase()
-    switch (humanChoice) {
-        case "rock":
-        case "r":
-            return "rock";
-        case "paper":
-        case "p":
-            return "paper";
-        case "scissors":
-        case "scissor":
-        case "s":
-            return "scissors";
-        default:
-            console.log("Invalid Input, Please try again.")
-            return getHumanChoice();
-    }
-} 
+const computerSelection = getcomputerSelection();
+
+
 
 // const playGame = () => {
-    let humanScore = 0;
-    let computerScore = 0;
 
-    let playRound = (humanChoice, computerChoice) => {
-        console.log(`Your choice: ${humanChoice}`);
-        console.log(`Computer's choice: ${computerChoice}`);
-        if (humanChoice === computerChoice) {
-            console.log("Tie");
-        } else if (humanChoice === "rock" && computerChoice === "scissors") {
-            console.log("You win! Rock beats Scissors.");
-            humanScore++;
-        } else if (humanChoice === "paper" && computerChoice === "rock") {
-            console.log("You win! Paper beats Rock.");
-            humanScore++;    
-        } else if (humanChoice === "scissors" && computerChoice === "paper") {
-            console.log("You win! Scissors beats Paper.");
-            humanScore++;
-        } else if (humanChoice === "rock" && computerChoice === "paper") {
-            console.log("You lose! Paper beats Rock.");
-            computerScore++;
-        } else if (humanChoice === "paper" && computerChoice === "scissors") {
-            console.log("You lose! Scissors beats Paper.");
-            computerScore++;
-        } else if (humanChoice === "scissors" && computerChoice === "rock") {
-            console.log("You lose! Rock beats Scissors.");
-            computerScore++;
-        } else {
-            console.log("Error");
+    let playRound = (humanSelection, computerSelection) => {
+        let messageContainer = document.querySelector(".message-container");
+        messageContainer.innerHTML = "";
+
+        let roundMsg = document.createElement("p");
+        roundMsg.innerText = `Round ${round}`;
+        roundMsg.style.fontWeight = "bold";
+        messageContainer.appendChild(roundMsg);
+
+        let humanSelectionMsg = document.createElement("p");
+        humanSelectionMsg.innerText = `Your choice: ${humanSelection}`
+        messageContainer.appendChild(humanSelectionMsg);
+
+        let computerSelectionMsg = document.createElement("p");
+        computerSelectionMsg.innerText = `Computer's choice: ${computerSelection}`
+        messageContainer.appendChild(computerSelectionMsg);
+
+        let roundResult = document.createElement("p");
+
+        if (humanSelection === computerSelection) {
+            roundResult.innerText = "Tie";
+            messageContainer.appendChild(roundResult);
         }
-        console.log(`Your score: ${humanScore}`);
-        console.log(`Computer score: ${computerScore}`);
+        if (
+            (humanSelection === "rock" && computerSelection === "scissors") ||
+            (humanSelection === "paper" && computerSelection === "rock") ||
+            (humanSelection === "scissors" && computerSelection === "paper")
+        ) {
+            roundResult.innerText = `You win! ${humanSelection} beats ${computerSelection}.`;
+            messageContainer.appendChild(roundResult);
+            humanScore++;
+        }
+
+        if (
+            (humanSelection === "rock" && computerSelection === "paper") ||
+            (humanSelection === "paper" && computerSelection === "scissors") ||
+            (humanSelection === "scissors" && computerSelection === "rock")
+        ) {
+            roundResult.innerText = `You lose! ${computerSelection} beats ${humanSelection}.`;
+            messageContainer.appendChild(roundResult);
+            computerScore++;
+        }
+
+            
+        let humanScoreMsg = document.createElement("p");
+        humanScoreMsg.innerText = `Your score: ${humanScore}`
+        messageContainer.appendChild(humanScoreMsg);
+
+        let computerScoreMsg = document.createElement("p");
+        computerScoreMsg.innerText = `Computer's score: ${computerScore}`
+        messageContainer.appendChild(computerScoreMsg);
+
+        round++;
     }
 
     // for (let i = 0; i < 5; i++) {
     //     console.log(`\n Round${i + 1}`)
-        const computerSelection = getComputerChoice();
     
         // playRound(humanSelection, computerSelection);
 
